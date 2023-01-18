bb18d7d4b3d27c1aec3f1f0c0d0bc639a60b2643
Layauuu <leo.couture@live.fr>
2023-01-18 16:34:24 +0100

    Update

--- maxi_prosit/lib/notweb.dart
+++ maxi_prosit/lib/notweb.dart
@@ -2,6 +2,8 @@ import 'dart:io' as io;
 import 'package:file_picker/file_picker.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:permission_handler/permission_handler.dart';
+//import 'package:universal_html/html.dart';
+import 'package:get/get.dart';
 
 class CreatePDFfile {
   final pw.Document _pdf;
@@ -10,16 +12,48 @@ class CreatePDFfile {
   CreatePDFfile(this._pdf, this._fileName);
 
   void create() async {
+    print(requestFilePermission().toString());
     print("Using notweb");
-    var status = await Permission.storage.status;
+    var status = await Permission.camera.status;
+    print(status);
+    if (!await Permission.contacts.request().isGranted) return;
     if (!status.isGranted) {
-      await Permission.storage.request();
+      await Permission.camera.request();
+      print("asked");
+    }
+    if (await Permission.storage.isPermanentlyDenied) {
+      // The user opted to never again see the permission request dialog for this
+      // app. The only way to change the permission's status now is to let the
+      // user manually enable it in the system settings.
+      openAppSettings();
     }
     String? path = await FilePicker.platform.getDirectoryPath();
     if (path == null) {
       return;
     }
+    print(path);
     final file = io.File("$path/$_fileName.pdf");
     await file.writeAsBytes(await _pdf.save());
   }
+}
+
+Future<bool> requestFilePermission() async {
+  PermissionStatus result;
+  // In Android we need to request the storage permission,
+  // while in iOS is the photos permission
+  if (GetPlatform.isAndroid) {
+    result = await Permission.storage.request();
+  } else {
+    result = await Permission.photos.request();
+  }
+
+  if (result.isGranted) {
+    //imageSection = ImageSection.browseFiles;
+    return true;
+  } else if (GetPlatform.isIOS || result.isPermanentlyDenied) {
+    //imageSection = ImageSection.noStoragePermissionPermanent;
+  } else {
+    //imageSection = ImageSection.noStoragePermission;
+  }
+  return false;
 }
